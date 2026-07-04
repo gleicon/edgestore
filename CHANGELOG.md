248c24499bfb60a228191924eebb5b32026c49a3
Gleicon Moraes <gleicon@gmail.com>
2026-07-04 12:43:12 -0300

    chore: bump version to 1.0.8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,20 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.8] - 2026-07-04
+
+### Added
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
+
 ## [1.0.7] - 2026-07-03
 
 ### Fixed
--- edgestore-cli/src/main.rs
+++ edgestore-cli/src/main.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 #[derive(Parser)]
 #[command(name = "edgestore-cli")]
 #[command(about = "EdgeStore database administration tool")]
-#[command(version = "1.0.7")]
+#[command(version = "1.0.8")]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
